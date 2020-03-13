5483bbaa918ce39543b58e8cf475edae76ae417c
Avin Sharma <sharmaavin@hotmail.com>
2020-03-12 23:37:34 -0400

    Ability to highlight and scroll up/down if the screen is big enough.

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ from colorama import init
 from authorize import get_token
 from collections import defaultdict
 
+from getch import getch
 from temp_token import token
 from colors import COLOR
 
@@ -16,9 +17,11 @@ api = todoist.TodoistAPI(token)
 api.sync()
 
 
-def display_all_tasks():
+def display_all_tasks(count, first=True):
     inbox_id = None
-    projects = {}   
+    projects = {}
+    everything = []
+
     for project in api.state['projects']:
         projects[project['id']] = project
         if hasattr(project, 'inbox_project'):
@@ -26,16 +29,40 @@ def display_all_tasks():
 
     project_tasks = group_tasks_into_projects(api.state['items'])  # project_id : List of tasks
     
+    if first:
+        spacing = '\n'
+    else:
+        spacing = '\033[B'
+
     for project_id in project_tasks:
         BOLD = COLOR['BOLD']
         RESET = COLOR['RESET']
         color = COLOR[projects[project_id]['color']]
         project_name = projects[project_id]['name']
-        print(f'\n{BOLD}{color}{project_name} {RESET}')
+        everything.append(f'{spacing}{BOLD}{color}{project_name} {RESET}')
 
         for task in project_tasks[project_id]:
             content = task['content']
-            print(f'- {color}{content}{RESET}')
+            everything.append(f'\u2610 {color}{content}{RESET}')
+    
+    if first:
+        end = '\n'
+        flush = False
+    else:
+        end = '\033[E \033[K'
+        flush = True
+    
+    
+
+    for i, line in enumerate(everything):
+        if i == len(everything) - count:
+            print(COLOR['REVERSED'] + line, end=end, flush=flush)
+        else:
+            print(line, end=end, flush=flush)
+
+    return len(everything) , len(projects)
+        
+
 
 def group_tasks_into_projects(tasks):
     project_tasks = defaultdict(list)
@@ -47,6 +74,34 @@ def group_tasks_into_projects(tasks):
 
 
 if __name__ == "__main__":
-    display_all_tasks()
+    n, spaces = display_all_tasks(0)
+    
+    count = 0
     while True:
-        print(input())
+        key = getch()
+
+        # print(key.encode('utf-8'))
+        if key == '\x03':
+            break
+
+        # Identify Arrow key presses
+        if key == '\033':
+            getch()
+            press = getch()
+            if press == 'A':
+                if count == n:
+                    continue
+
+                print(f'\033[{n + spaces}A', end='', flush = True)
+                count += 1 
+                display_all_tasks(count, False)
+            if press == 'B':
+                if count == 1:
+                    continue
+                
+                print(f'\033[{n + spaces}A', end='')
+                count -= 1 
+                display_all_tasks(count, False)
+        
+        
+        
